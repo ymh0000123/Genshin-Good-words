5e1d7aa019eaba175fa112fe8780983b60ad05cc
没用的小废鼠 <107793048+ymh0000123@users.noreply.github.com>
2023-11-12 17:04:05 +0800

    修复

--- script_jsdeliv.js
+++ script_jsdeliv.js
@@ -1,3 +1,9 @@
+/*
+ * @Date: 2023-11-11 19:58:13
+ * @LastEditors: 
+ * @LastEditTime: 2023-11-12 16:58:45
+ * @FilePath: \undefinedd:\hc\Genshin-Impact-Good-words-and-sentences\script_jsdeliv.js
+ */
 // 使用fetch函数获取文本文件内容
 fetch('https://cdn.jsdelivr.net/gh/ymh0000123/Genshin-Impact-Good-words-and-sentences@master/Good-words.txt')
 .then(response => response.text())
@@ -9,5 +15,5 @@ fetch('https://cdn.jsdelivr.net/gh/ymh0000123/Genshin-Impact-Good-words-and-sent
   document.getElementById('ys').innerText = randomLine;
 })
 .catch(error => {
-  console.error('Error fetching yshj:', error);
+  console.error('Error fetching ys:', error);
 });

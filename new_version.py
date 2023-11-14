f879a13527a36e0d18ae224b977cc29e417cc416
没用的小废鼠 <107793048+ymh0000123@users.noreply.github.com>
2023-11-14 20:29:08 +0800

    更新

--- new_version.py
+++ new_version.py
@@ -1,12 +1,22 @@
-# new_version.py
+import json
 
-import json;
-import time;
+# 读取package.json文件
+with open('package.json', 'r') as file:
+    data = json.load(file)
 
-with open("package.json",'r',encoding='utf-8') as f:
-    jspack = json.load(f)
+# 解析version字段
+version = data['version']
+major, minor, patch = map(int, version.split('.'))
 
-new_version = '1.1.'+str(int(time.time()))[1:11:1]
-jspack['version']=new_version
-with open("package.json",'w',encoding='utf-8') as f:
-    json.dump(jspack, f,ensure_ascii=False)
+# 递增version
+patch += 1
+new_version = f'{major}.{minor}.{patch}'
+
+# 更新package.json中的version字段
+data['version'] = new_version
+
+# 将修改后的数据写回package.json文件
+with open('package.json', 'w') as file:
+    json.dump(data, file, indent=2)
+
+print(f'版本号已更新为 {new_version}')

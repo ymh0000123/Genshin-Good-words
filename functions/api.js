321cbb5b98c114347bd98435d0aab9f507eb947a
ymh0000123 <ymh0000123@163.com>
2024-07-03 12:54:41 +0800

    api

--- functions/api.js
+++ functions/api.js
@@ -1,4 +1,4 @@
-export async function onRequest(context) {
+export async function onRequest(event) {
   const url = 'https://unpkg.com/genshin-good-words/Good-words.txt';
   const response = await fetch(url);
   const text = await response.text();
@@ -9,7 +9,14 @@ export async function onRequest(context) {
   // Get a random line
   const randomLine = lines[Math.floor(Math.random() * lines.length)];
 
-  return new Response(randomLine, {
-    headers: { 'content-type': 'text/plain' },
+  // Prepare JSON response
+  const jsonResponse = {
+    quote: randomLine,
+  };
+
+  return new Response(JSON.stringify(jsonResponse), {
+    headers: {
+      'Content-Type': 'application/json; charset=utf-8',
+    },
   });
 }

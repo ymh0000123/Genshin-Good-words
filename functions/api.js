1e116423d7df49a949c9ac414bf5e367bf2e658d
ymh0000123 <ymh0000123@163.com>
2024-08-03 17:38:41 +0800

    Revert "1"
    This reverts commit dfb68d76d7a04c66a247302bbc51afb74ced852f.

--- functions/api.js
+++ functions/api.js
@@ -1,31 +1,23 @@
 addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request))
-})
+  event.respondWith(handleRequest(event.request));
+});
 
 async function handleRequest(request) {
-  // 定义从 URL 获取内容的地址
-  const url = 'https://genshin-good-words.pages.dev/Good-words.txt'
+  const lines = await fetchRandomLine();
+  const randomLine = getRandomLine(lines);
 
-  // 发起请求获取内容
-  const response = await fetch(url)
-  const text = await response.text()
+  return new Response(randomLine, {
+    headers: { 'content-type': 'text/plain' },
+  });
+}
 
-  // 将内容拆分为行
-  const lines = text.split('\n').filter(line => line.trim() !== '')
+async function fetchRandomLine() {
+  const response = await fetch('https://genshin-good-words.pages.dev/Good-words.txt');
+  const text = await response.text();
+  return text.split('\n').filter(line => line.trim() !== '');
+}
 
-  // 随机选择一行
-  const randomIndex = Math.floor(Math.random() * lines.length)
-  const randomLine = lines[randomIndex]
-
-  // 构建 JSON 响应
-  const jsonResponse = {
-    goodWord: randomLine
-  }
-
-  // 返回 JSON 响应
-  return new Response(JSON.stringify(jsonResponse), {
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  })
+function getRandomLine(lines) {
+  const randomIndex = Math.floor(Math.random() * lines.length);
+  return lines[randomIndex];
 }
